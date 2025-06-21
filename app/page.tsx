dd8f153416a1cee8615629e6359c5b227e01960d
Shereni <info@aewebsites.co.zw>
2025-06-21 16:14:52 +0200

    Tested the themes in globals (fonts and colors)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className='h-screen flex font-robert-r text-center items-center justify-center'>
+    <div className='h-screen flex 4xl text-blue-300 font-robert-r text-center items-center justify-center'>
       <h1 className='text-4xl'>Hello World!</h1>
+      <h1 className='text-4xl text-blue-gaming'>Hello world</h1>
     </div>
   );
 }
