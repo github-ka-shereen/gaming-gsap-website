91a5cb3a3689944cb8e136a5e91d950f00a56eb3
Shereni <info@aewebsites.co.zw>
2025-06-22 14:30:10 +0200

    Font adjustment

--- components/About.tsx
+++ components/About.tsx
@@ -29,7 +29,7 @@ function About() {
   return (
     <div id='about' className='min-h-screen w-screen'>
       <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-        <h2 className='font-general text-sm uppercase md:text-[10px]'>
+        <h2 className='font-general text-sm uppercase md:text-[28px]'>
           Welcome To Zentry
         </h2>
         <AnimatedTitle
