fafa88c197d1369c3bf8f5e94751d05776a6d5fb
Shereni <info@aewebsites.co.zw>
2025-06-22 11:21:23 +0200

    Implement Hero section no gsap

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
-    <div className='h-screen flex 4xl text-blue-300 font-robert-r text-center items-center justify-center'>
-      <h1 className='text-4xl'>Hello World!</h1>
-      <h1 className='text-4xl text-blue-gaming'>Hello world</h1>
-    </div>
+    <main className='max-width'>
+      <Hero />
+    </main>
   );
 }
